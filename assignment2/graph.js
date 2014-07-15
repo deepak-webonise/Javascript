b0500d651eaefd77ff8d9a13718f59fdc12a8ae9
deepak-webonise <deepak.kabbur@weboniselab.com>
2014-07-15 18:52:33 +0530

    Graph working for co ordinate values less than 5

--- assignment2/graph.js
+++ assignment2/graph.js
@@ -1,14 +1,11 @@
 function Canvasapp2(step,startp,endp){
+    //assigning the values to variables
     this._step =step;
     this._startp =startp;
     this._endp =endp;
     this._c=document.getElementById("canvas");
     this._ctx=this._c.getContext("2d");
     
-
-
-    //alert(this._eqn);
-
 }
 Canvasapp2.prototype.set=function(x,y,cst,op){
     this._x = x ;
@@ -18,11 +15,11 @@ Canvasapp2.prototype.set=function(x,y,cst,op){
 }
 
 Canvasapp2.prototype.drawGrid2=function(){
-    //draw vertical line
+    //draw vertical and horizantal lines
     for (i=0;i<=this._c.width;i+=(this._c.width)/10)
     {
 
-        if (i == (this._c.width)/2) // different colors for origin lines
+        if (i == (this._c.width)/2) // setting blue color for origin lines
             {
                 this._ctx.lineWidth = 3; // line width
                 this._ctx.strokeStyle = 'blue'; //line color
@@ -66,54 +63,60 @@ Canvasapp2.prototype.drawGrid2=function(){
 Canvasapp2.prototype.drawLine2=function(){
       
         var ycof=[],xcof=[];
-        for(var i=0; i< 5;i++)
+        //setting start and end point for equation
+        for(var i=this._startp; i<this._endp;i+=this._step)
         {
            //finiding the co-ordinates of x and y coefficients.
            switch(this._op) {
                                    //finding the cordinates of y coefficient depending on operator 
                                     case "+":
                                              ycof[i] = eval((this._cst - (this._x * i ))/this._y);
+                                             xcof[i] = i;
                                               break;
                                     case "-":
                                              ycof[i] = eval((this._cst - (this._x * i ))/(-this._y));
+                                             xcof[i] = i;
                                               break;
                                     case "*":
                                              ycof[i] = eval(this._cst / ((this._x * i )*this._y));
+                                             xcof[i] = i;
                                               break;
                                     case "/":
                                              ycof[i] = eval((this._x * i )/(this._cst * this._y));
+                                             xcof[i] = i;
                                               break;
   
                                     default:
                                             alert("Invalid Operator ");
                                             break;
                                 }
- 
-            //adding x co-oedinates 
-            xcof[i] = i;
-            
+                     
         }
+        //Display X and Y cordinates
         alert("X cordinates"+xcof+"\nY cordinates"+ycof);
-        
+        //setting the font, baseline and stroke color for graph
         this._ctx.font = '15px _sans';
         this._ctx.textBaseline = 'bottom';
-        this._ctx.strokeStyle='blue';
+        this._ctx.strokeStyle='green';
+        this._ctx.lineWidth=2;
+
+        this._ctx.moveTo(xcof[this._startp] * (this._c.width/10), ycof[this._startp] * (this._c.width/10));
         for(var i=-5; i< 5;i++)
         {
                     
                 for(var j=-5; j<5;j++)
                 {
                         //validating x,y co-ordinates(equatio) with i,j co-ordinates (graph)
-                        if((j * (this._c.width/10)) ==(ycof[i] * (this._c.width/10)) && (i * (this._c.width/10)) == (xcof[i])* (this._c.width/10))
+                        if((j * (this._c.width/10)) == (ycof[i] * (this._c.width/10)) && (i * (this._c.width/10)) == (xcof[i])* (this._c.width/10))
                         {
                             //fill a rectangle
                             this._ctx.fillRect(xcof[i] * (this._c.width/10),-ycof[i] * (this._c.width/10),10,10); 
                             //fill text with (x,y) co-ordinates
                             this._ctx.fillText ("("+xcof[i].toString()+","+ycof[i].toString()+")", xcof[i] * (this._c.width/10), -ycof[i] * (this._c.width/10));
 
-                            this._ctx.moveTo(xcof[i] * (this._c.width/10), -ycof[i] * (this._c.width/10));
-                             this._ctx.lineTo(xcof[++i] * (this._c.width/10), -ycof[++i] * (this._c.width/10));
-                             this._ctx.stroke();
+                            //draw line
+                             this._ctx.lineTo(xcof[i] * (this._c.width/10), -ycof[i] * (this._c.width/10));
+                             
                                                        
 
                         }
@@ -121,20 +124,23 @@ Canvasapp2.prototype.drawLine2=function(){
 
 
                  }
-        }    
+        } 
+        this._ctx.stroke();   
             
 
 }
 function get()
 {
-var eqn = document.getElementById("eqn").value;
-var step = document.getElementById("steps").value;
-var startp = document.getElementById("startp").value;
-var endp = document.getElementById("endp").value;
-eqn = eqn.replace(/ +/g, "");
-var xc=0,yc=0,cst=0;
-var i =0;
-
+    //Fetching the values from input boxes
+    var eqn = document.getElementById("eqn").value;
+    var step = document.getElementById("steps").value;
+    var startp = document.getElementById("startp").value;
+    var endp = document.getElementById("endp").value;
+    //Removing spaces from equation
+    eqn = eqn.replace(/ +/g, "");
+    var xc=0,yc=0,cst=0;
+    var i =0;
+        //parsing X coefficient
         while(eqn[i]!='x')
         {
             xc+=eqn[i];
@@ -142,7 +148,7 @@ var i =0;
         }
         var op = eqn[++i];
         i++;
-        
+        //parsing Y coefficient
         while(eqn[i]!='y')
         {
             yc+=eqn[i];
@@ -151,20 +157,19 @@ var i =0;
 
         i++;
         i++;
-       
+       //parsing constant
         while(i < eqn.length){
 
             cst+=eqn[i];
             i++;
            
         }
-
-var g = new Canvasapp2(step,startp,endp);
-alert(step);
+//creating object of Canvasapp2 function
+var g = new Canvasapp2(step-0,startp-0,endp-0);
+alert("Scale ="+step);
 g.set(xc-0,yc-0,cst-0,op);
 g.drawGrid2();
 g.drawLine2();
 
 }
-
 
